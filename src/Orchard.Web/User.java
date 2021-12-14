77ad23176011b7f448336babbba0a1c8378b39bf
Idan Elor <idanelor@apiiro.com>
2021-12-14 13:33:31 +0200

    add data

---
+++ src/Orchard.Web/User.java
@@ -0,0 +1,10 @@
+public class BankAccounts
+{
+    public String id;
+    public String fullName;
+    public String birthDate;
+    public int accountType;
+    public String account;
+    public double balance;
+​
+}
